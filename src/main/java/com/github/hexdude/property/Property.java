73bbc6b258ad34839dc10986b7c7ead1deb64642
HEXDude <ZJakeZzz@Outlook.com>
2022-03-20 15:11:08 +0800

    Refactored Property.

--- src/main/java/com/github/hexdude/property/Property.java
+++ src/main/java/com/github/hexdude/property/Property.java
@@ -8,82 +8,79 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @description Accept the corresponding value from configuration file.
  * @version beta-0.1
  */
-@ConfigurationProperties(prefix = "hex.snowflake")
+@ConfigurationProperties(prefix = "hex.snowflake", ignoreInvalidFields = true)
 public class Property {
 
     /**
-     * Length of Worker ID in generated 64 bit ID.<br>
-     * Worker ID在64位ID中所占位数
+     * Defined the binary length of {@link #workerId} occupied in generated 64bit unique ID.<br>
+     * 指定{@link #workerId}的二进制数值在生成的64位雪花算法ID中所占的位数。
      */
     private static final long WORKER_ID_LENGTH = 5L;
 
     /**
-     * The maximum decimal value of Worker ID.<br>
-     * Worker ID最大值
-     */
-    @SuppressWarnings("PointlessBitwiseExpression")
-    private static final long WORKER_ID_MAXIMUM = -1L ^ (-1L << WORKER_ID_LENGTH);
-
-    /**
-     * Length of Data-Center ID in generated 64 bit ID.<br>
-     * Data-Center ID在64位ID中所占位数
+     * Defined the binary length of {@link #dataCenterId} occupied in generated 64bit unique ID.<br>
+     * 指定{@link #dataCenterId} ID的二进制数值在生成的64位雪花算法ID中所占的位数。
      */
     private static final long DATA_CENTER_ID_LENGTH = 5L;
 
     /**
-     * The maximum decimal value of Data-Center ID.<br>
-     * Data-Center ID最大值
+     * Defined the maximum acceptable {@link #workerId} AKA {@code 0L~63L}.<br>
+     * 指定可接受的最大的{@link #workerId}的值为{@code 0L~63L}。
      */
-    @SuppressWarnings({"PointlessBitwiseExpression", "unused"})
-    private static final long DATA_CENTER_ID_MAXIMUM = -1L ^ (-1L << DATA_CENTER_ID_LENGTH);
+    @SuppressWarnings({"unused", "PointlessBitwiseExpression"})
+    private static final long MAXIMUM_WORKER_ID = -1L ^ (-1L << WORKER_ID_LENGTH);
 
     /**
-     * Worker ID is the only two of the customize attributes in Snowflake strategy.<br>
-     * It is deeply influences the final generated value.<br>
-     * Worker ID是唯二可以配置的属性
+     * Defined the maximum acceptable {@link #dataCenterId} AKA {@code 0L~63L}.<br>
+     * 指定可接受的最大的{@link #dataCenterId}的值为{@code 0L~63L}。
      */
-    @SuppressWarnings("AlibabaLowerCamelCaseVariableNaming")
-    private Long workerID = 1L;
+
+    @SuppressWarnings({"unused", "PointlessBitwiseExpression"})
+    private static final long MAXIMUM_DATA_CENTER_ID = -1L ^ (-1L << DATA_CENTER_ID_LENGTH);
 
     /**
-     * Data-Center ID is the only two of the customize attributes in Snowflake strategy.<br>
-     * It is deeply influences the final generated value.<br>
-     * Data-Center ID是唯二可以配置的属性
+     * Default value is {@code 1645539742L} AKA {@code 2022/02/22 22:22:22}.<br>
+     * Defined the start time of SnowFlake system's generating strategy.<br>
+     * Not recommended modifying this attribute.<br>
+     * 定义雪花算法生成策略的起始时间。
      */
-    @SuppressWarnings("AlibabaLowerCamelCaseVariableNaming")
-    private Long dataCenterID = 1L;
-
-    @SuppressWarnings("AlibabaLowerCamelCaseVariableNaming")
-    public Long getWorkerID() {
-        return workerID;
-    }
-
-    @SuppressWarnings("AlibabaLowerCamelCaseVariableNaming")
-    public void setWorkerID(Long workerID) {
-        checkRange(workerID);
-        this.workerID = workerID;
-    }
-
-    @SuppressWarnings("AlibabaLowerCamelCaseVariableNaming")
-    public Long getDataCenterID() {
-        return dataCenterID;
-    }
-
-    @SuppressWarnings("AlibabaLowerCamelCaseVariableNaming")
-    public void setDataCenterID(Long dataCenterID) {
-        checkRange(dataCenterID);
-        this.dataCenterID = dataCenterID;
-    }
+    private long startTime = 1645539742L;
 
     /**
-     * Check if the param over {@link #WORKER_ID_LENGTH} or {@link #DATA_CENTER_ID_LENGTH}.<br>
-     * @param ID Long
-     * @throws IllegalArgumentException If param is not fit the range.
+     * Default value is {@code 0L}.<br>
+     * Defined the current service instance's identifier of the entire microservice system.<br>
+     * 定义当前服务在整个微服务系统中的唯一标识。
      */
-    @SuppressWarnings("AlibabaLowerCamelCaseVariableNaming")
-    protected void checkRange(Long ID) throws IllegalArgumentException  {
-        if (ID < 0 || ID > WORKER_ID_MAXIMUM) {
-            throw new IllegalArgumentException("Snowflake init failure,Worker ID or Data-Center ID may over range!");
-        }
+    private long workerId = 0L;
+
+    /**
+     * Default value is {@code 0L}.<br>
+     * Defined the current service instance's location of which server's[dataCenter] identifier.<br>
+     * 定义当前服务所部署的数据中心的唯一标识。
+     */
+    private long dataCenterId = 0L;
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getWorkerId() {
+        return workerId;
+    }
+
+    public void setWorkerId(long workerId) {
+        this.workerId = workerId;
+    }
+
+    public long getDataCenterId() {
+        return dataCenterId;
+    }
+
+    public void setDataCenterId(long dataCenterId) {
+        this.dataCenterId = dataCenterId;
     }
 }
